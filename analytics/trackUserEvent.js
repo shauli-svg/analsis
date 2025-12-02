190bafe62d9ba82149017c82b3eb1debc901209d
shauli-svg <shauli.barda@gmail.com>
2025-12-02 13:42:10 +0200

    Update trackUserEvent.js

--- analytics/trackUserEvent.js
+++ analytics/trackUserEvent.js
@@ -1,1 +1,26 @@
+// analytics/trackUserEvent.js
+import { query } from "../db.js";
+
+/**
+ * רושם אירוע אנליטי לטבלת user_events
+ */
+export async function trackUserEvent({
+  userId = null,
+  name,
+  context = {},
+  sessionId = null,
+  source = "web",
+}) {
+  if (!name) {
+    throw new Error("trackUserEvent: name is required");
+  }
+
+  await query(
+    `
+      INSERT INTO user_events (user_id, event_name, context, session_id, source)
+      VALUES ($1, $2, $3, $4, $5)
+    `,
+    [userId, name, context, sessionId, source]
+  );
+}
 
